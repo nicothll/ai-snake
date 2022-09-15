6be9092155e08250f114425068c994481ad5dd6c
Nicolas Thuillier <nicothll.dev@gmail.com>
2022-09-15 18:21:25 -0500

    renamed game and updated

--- game.py
+++ game.py
@@ -8,7 +8,6 @@ import numpy as np
 pygame.init()
 
 SCORE_FONT = pygame.font.Font("./fonts/arial.ttf", 25)
-GAME_OVER_FONT = pygame.font.Font("./fonts/arial.ttf", 60)
 
 
 class Direction(Enum):
@@ -88,7 +87,7 @@ class SnakeGameAI:
         reward = 0  # eat_food = +10, game_over = -10, else = 0
         game_over = False
 
-        if self._collision() or self.frame_iteration > 100 * len(self.snake):
+        if self.is_collision() or self.frame_iteration > 100 * len(self.snake):
             game_over = True
             reward = -10
             return reward, game_over, self.score
@@ -108,7 +107,7 @@ class SnakeGameAI:
         # 6. Return Game Over and Score
         return game_over, self.score
 
-    def _collision(self, pt=None):
+    def is_collision(self, pt=None):
         if pt is None:
             pt = self.head
         # Hits boundaries
